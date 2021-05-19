798d1c186c63301937b7308d8af49aa607b57277
TarunSaini063 <tsaini063@outlook.com>
2021-05-19 14:31:54 +0530

    Day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_starter/pages/login-page.dart';
 
-import 'MyHomePage.dart';
+import 'pages/MyHomePage.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,13 +13,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      themeMode: ThemeMode.light,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Hello World'),
+      darkTheme: ThemeData(
+          primarySwatch: Colors.orange
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/" : (context) => LoginPage(title: "Login Page",),
+        "/login" : (context) => LoginPage(title: "Login Page",),
+        "/home" : (context) => MyHomePage(title: "Home Page",),
+      },
     );
   }
 }
-
-
-
