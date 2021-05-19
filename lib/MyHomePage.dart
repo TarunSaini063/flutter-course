8e15d2cb4c1698bd3ce047984303580c35798324
TarunSaini063 <tsaini063@outlook.com>
2021-05-19 13:27:33 +0530

    Flutter day 1

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  final title;
+  const MyHomePage({Key key,this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('win95'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Title of this app is $title',style: TextStyle(fontSize: 24,color: Colors.blueAccent),),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
