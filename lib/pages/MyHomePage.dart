4cbd61d3e8d8c7d6df5ef0234a0b5c7fd7f585d3
TarunSaini063 <tsaini063@outlook.com>
2021-05-20 13:23:13 +0530

    create list view template

--- lib/pages/MyHomePage.dart
+++ lib/pages/MyHomePage.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_starter/models/Catalog.dart';
+import 'package:flutter_starter/widgets/ItemWidget.dart';
 import 'package:flutter_starter/widgets/MyDrawer.dart';
 
 class MyHomePage extends StatelessWidget {
   final title;
-  const MyHomePage({Key key,this.title}) : super(key: key);
-
+  MyHomePage({Key key,this.title}) : super(key: key);
+  final dummyList = List.generate(50, (index) => products[0]);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('win95'),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Title of this app is $title',style: TextStyle(fontSize: 24,color: Colors.blue),),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context,index){
+            return ItemWidget(item: dummyList[index]);
+          },
+          ),
       ),
       drawer: MyDrawer(),
     );
