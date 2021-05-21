41ba7aa851b6ed7279a2b8961a7b30e1f7b80b49
TarunSaini063 <tsaini063@outlook.com>
2021-05-21 14:18:18 +0530

    divide homepage in separate modules

---
+++ lib/widgets/home_widgets/CatalogHeader.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_starter/widgets/MyThemes.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'package:flutter/material.dart';
+class CatalogHeader extends StatelessWidget {
+  const CatalogHeader({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalog App".text.xl5.bold.color(MyThemes.darkBluishColor).make(),
+        "Trending products".text.make()
+      ],
+    );
+  }
+}
